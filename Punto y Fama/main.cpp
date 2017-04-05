97440d61a7983ca7026d15968fdb458fcd66123d
Franz Pichardo Rodriguez <franz.pichardo.r@gmail.com>
2017-04-04 21:55:30 -0400

    El programa chequea si el numero digitado se repite

--- Punto y Fama/main.cpp
+++ Punto y Fama/main.cpp
@@ -42,27 +42,79 @@ int getRandom()
     return random;
 }
 
-int main()
+int checkNum(int x)
 {
-    int x, arrayx[4], randomNum, randomarray[4];
-
-    randomNum = getRandom();
-
-    cout << randomNum << endl;
-
-    cout << "Digite el numero de 4 digitos: " << endl;
-    while(x<1000 || x>9999)
-    {
-        cin >> x;
-        if(x<1000 || x>9999)
-            cout << "El numero debe ser de 4 digitos." << endl;
-    }
+    int arrayx[4];
 
     arrayx[0] = x/1000;
     arrayx[1] = (x/100)%10;
     arrayx[2] = (x/10)%10;
     arrayx[3] = x%10;
 
-    cout << endl;
+    for(int i=0; i<4; i++){
+        for(int k=0; k<4; k++)
+        {
+            if(i==k)
+                continue;
+            else if(arrayx[i] == arrayx[k])
+            {
+                return 0;
+            }
+        }
+    }
+    return x;
+}
+
+int main()
+{
+    int x, arrayx[4], randomNum, randomarray[4], counter=0, newx;
+
+    randomNum = getRandom();
+
+    randomarray[0] = randomNum/1000;
+    randomarray[1] = (randomNum/100)%10;
+    randomarray[2] = (randomNum/10)%10;
+    randomarray[3] = randomNum%10;
+
+    cout << randomNum << endl;
+
+    for(counter=0; counter<10; counter++)
+    {
+        do
+        {
+            cout << "Digite el numero de 4 digitos: " << endl;
+            cin >> x;
+            if(x<1000 || x>9999)
+            {
+                cout << "El numero debe ser de 4 digitos." << endl;
+                counter--;
+            }
+            newx = checkNum(x);
+
+            if(newx!=x)
+                x=newx;
+
+            if(x==0)
+            {
+                cout << "El numero tiene digitos repetidos." << endl;
+                counter--;
+            }
+        }
+        while(x<1000 || x>9999 || x==0);
+
+        arrayx[0] = x/1000;
+        arrayx[1] = (x/100)%10;
+        arrayx[2] = (x/10)%10;
+        arrayx[3] = x%10;
+
+        for(int i=0; i<4; i++)
+            for(int k=0; k<4; k++)
+                if(arrayx[i] == randomarray[k])
+                {
+
+                }
+    }
+    cout << counter << endl;
+
 
 }
